27f699af924d3f5878748f75adf9ad07182c5c7b
aspirin <asp1r1n0@yandex.ru>
2015-05-02 00:48:20 +0400

    Added: group validation method, login rule, email rule

--- components/helpers/validation.php
+++ components/helpers/validation.php
@@ -5,13 +5,29 @@ defined('APP_PATH') or die('Access denied!');
 /**
  * Validation methods
  *
- * @since 0.1
+ * @since 0.2
  * @author Bogomazov Bogdan (ASPIRIN++) <b.bogomazov@gamil.com>
  */
 return [
-    'helpers:validation:_ver' => '0.1',
-    'helpers:validation:check' => function ($value, $rule) {
-        
+    'helpers:validation:_ver' => '0.2',
+    'helpers:validation:check' => function ($arr, $rules) {
+        $res = [];
+        foreach ($rules as $rule) {
+            if (!isset($arr[$rule[0]]) && $rule[1] !== 'not_empty') {
+                continue;
+            }
+            $val = isset($arr[$rule[0]]) ? $arr[$rule[0]] : NULL;
+
+            $params = isset($rule[2]) && is_array($rule[2]) ? $rule[2] : [];
+            $val = call_user_func_array('f', array_merge(["helpers:validation:{$rule[1]}", $val], $params));
+            if(!$val) {
+                if(!isset($res[$rule[0]])) {
+                    $res[$rule[0]] = [];
+                }
+                $res[$rule[0]][] = $rule[1];
+            }
+        }
+        return ['success' => empty($res), 'errors' => $res];
     },
     'helpers:validation:not_empty' => function($val) {
         return !in_array($val, array(NULL, FALSE, '', array()), TRUE);
@@ -25,15 +41,15 @@ return [
     'helpers:validation:max_length' => function($val, $len) {
         return f('helpers:string:utf8_strlen', $val) <= $len;
     },
-   /**
-    * 
-    * @see http://www.w3.org/Protocols/rfc822/
-    * @see http://www.iamcal.com/publish/articles/php/parsing_email/
-    *
-    * @param   string  $email  email address
-    * @param   boolean $strict strict RFC compatibility
-    * @return  boolean
-    */
+    /**
+     * Email validation
+     * @see http://www.w3.org/Protocols/rfc822/
+     * @see http://www.iamcal.com/publish/articles/php/parsing_email/
+     *
+     * @param   string  $email  email address
+     * @param   boolean $strict strict RFC compatibility
+     * @return  boolean
+     */
     'helpers:validation:email' => function($email, $strict = FALSE) {
         if (f('helpers:string:utf8_strlen', $email) > 254) {
             return FALSE;
@@ -59,20 +75,47 @@ return [
 
         return (bool) preg_match($expression, (string) $email);
     },
+    /**
+     * Alphabet
+     */
     'helpers:validation:alpha' => function($str, $utf8 = FALSE) {
         $str = (string) $str;
         return $utf8 === TRUE ? (bool) preg_match('/^\pL++$/uD', $str) : ctype_alpha($str);
     },
+    /**
+     * Latin alphabet, numbers, symbols "_" and "-" (great for login)
+     */
+    'helpers:validation:login' => function($str) {
+        return !preg_match('/[^(\w)|(\_)|(\-)]/', (string) $str);
+    },
+    /**
+     * Latin alphabet, numbers, symbols: "_", "-", "." (great for email login)
+     */
+    'helpers:validation:email_login' => function ($str) {
+        return !preg_match('/[^(\w)|(\_)|(\.)|(\-)]/', (string) $str);
+    },
+    /**
+     * Alphabet & numbers
+     */
     'helpers:validation:alpha_numeric' => function($str, $utf8 = FALSE) {
         return $utf8 === TRUE ? (bool) preg_match('/^[\pL\pN]++$/uD', $str) : ctype_alnum($str);
     },
+    /**
+     * Alphabet & dash
+     */
     'helpers:validation:alpha_dash' => function($str, $utf8 = FALSE) {
         $regex = $utf8 === TRUE ? '/^[-\pL\pN_]++$/uD' : '/^[-a-z0-9_]++$/iD';
         return (bool) preg_match($regex, $str);
     },
+    /**
+     * Digit
+     */
     'helpers:validation:digit' => function($str, $utf8 = FALSE) {
         return $utf8 === TRUE ? (bool) preg_match('/^\pN++$/uD', $str) : (is_int($str) AND $str >= 0) OR ctype_digit($str);
     },
+    /**
+     * Digit or decimal
+     */
     'helpers:validation:numeric' => function($str) {
         list($decimal) = array_values(localeconv());
         return (bool) preg_match('/^-?+(?=.*[0-9])[0-9]*+'.preg_quote($decimal).'?+[0-9]*+$/D', (string) $str);
